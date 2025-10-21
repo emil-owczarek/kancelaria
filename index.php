79148a57525a8638928093aee70ad043a34c8c6a
Emil <eowczarek@1r.agency>
2025-10-21 23:38:51 +0200

    test actions v4

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php
-get_header();
 ?>
-<h1>Witaj w moim !!!!!!</h1>
-<?php
-get_footer();
-?>
+
+<main id="primary" class="site-main">
+    <div class="container">
+        <h1>Witaj w Kancelarii Prawnej Pawła Noworolnika</h1>
+        <p>Profesjonalne usługi prawne na najwyższym poziomie.</p>
+        
+        <?php
+        if (have_posts()) :
+            while (have_posts()) :
+                the_post();
+                ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php the_title('<h2 class="entry-title">', '</h2>'); ?>
+                    </header>
+
+                    <div class="entry-content">
+                        <?php
+                        the_content();
+                        ?>
+                    </div>
+                </article>
+                <?php
+            endwhile;
+        else :
+            ?>
+            <p>Brak treści do wyświetlenia.</p>
+            <?php
+        endif;
+        ?>
+    </div>
+</main>
+
